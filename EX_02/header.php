6bdcd96402ee98cb7c4e5389a478d53d60c8ab6d
Noraelk <67266165+Noraelk@users.noreply.github.com>
2020-06-29 13:02:35 +0200

    DAY 05

---
+++ EX_02/header.php
@@ -0,0 +1,11 @@
+<header>
+<div class="navigation">
+            <?php
+            include("navigation.php")
+            ?>
+        </div>
+        <h1 class="titre">
+            Summer Code Camp
+        </h1>
+
+    </header>
